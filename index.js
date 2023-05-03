6183696134dfd247f6797a12d890bf6ec50074db
Kerry Guarino <kerry.guarino@gmail.com>
2023-05-03 10:55:07 -0400

    lab complete - struggled

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+
+function superbowlWin(array){
+   const found = array.find(element => element.result === "W")
+      if (found){
+         return (found.year)
+      }
+   }
+
+
